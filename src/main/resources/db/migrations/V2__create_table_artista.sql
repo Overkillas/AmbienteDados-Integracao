0d5848c47a64dd7474812e5e3232b8a9d0e82377
Felipe Lins <felipeplinsesilva@gmail.com>
2024-11-29 12:12:27 -0300

    tirei "nome" de artista

--- src/main/resources/db/migrations/V2__create_table_artista.sql
+++ src/main/resources/db/migrations/V2__create_table_artista.sql
@@ -1,6 +1,5 @@
 CREATE TABLE IF NOT EXISTS Artista (
 	id_artista INT NOT NULL AUTO_INCREMENT,
-    nome VARCHAR(255) NOT NULL,
     nome_artistico VARCHAR(255) NOT NULL,
     biografia TEXT NOT NULL,
     pais_origem VARCHAR(255) NOT NULL,
